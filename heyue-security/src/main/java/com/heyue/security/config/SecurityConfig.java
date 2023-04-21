0efdbe42f7b9dbce26d7b7633cb1cb516047f179
1995huanyuye <2256219118@qq.com>
2023-04-22 00:16:44 +0800

    跨域处理

--- heyue-security/src/main/java/com/heyue/security/config/SecurityConfig.java
+++ heyue-security/src/main/java/com/heyue/security/config/SecurityConfig.java
@@ -13,19 +13,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.time.Duration;
-import java.util.Collections;
 
 /**
  * SpringSecurity相关配置，仅用于配置SecurityFilterChain
  */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig {
     @Autowired
     private IgnoreUrlsConfig ignoreUrlsConfig;
     @Autowired
@@ -73,38 +67,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
             registry.and().addFilterBefore(dynamicSecurityFilter, FilterSecurityInterceptor.class);
         }
         return httpSecurity.build();
-    }
-
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .permitAll()
-                .and()
-                .formLogin()
-                .permitAll()
-                .and()
-                .httpBasic()
-                .and()
-                //支持跨域访问
-                .cors()
-                .configurationSource(corsConfigurationSource())
-                .and()
-                .csrf()
-                .disable();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
-        configuration.setAllowedMethods(Collections.singletonList("*"));
-        configuration.setAllowedHeaders(Collections.singletonList("*"));
-        configuration.setMaxAge(Duration.ofHours(1));
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
